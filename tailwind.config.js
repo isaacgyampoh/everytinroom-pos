33dd01265b8edd23ca1c6a0202e8036faf19dd70
isaac gyampoh <isaacgyampoh@users.noreply.github.com>
2026-02-26 22:20:35 +0000

    Change brand color to Cambridge red (#c41e2a)

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ export default {
     extend: {
       fontFamily: { outfit: ['Outfit', 'system-ui', 'sans-serif'] },
       colors: {
-        brand: { 50: '#f0f9ff', 100: '#e0f2fe', 200: '#bae6fd', 300: '#7dd3fc', 400: '#38bdf8', 500: '#0ea5e9', 600: '#0284c7', 700: '#0369a1', 800: '#075985', 900: '#0c4a6e' },
+        brand: { 50: '#fef2f2', 100: '#fee2e2', 200: '#fecaca', 300: '#f87171', 400: '#e53e3e', 500: '#c41e2a', 600: '#b11a24', 700: '#9b1520', 800: '#7f1119', 900: '#5c0c12' },
         wa: '#25d366',
         momo: '#f59e0b',
         paystack: '#00c3f7',
