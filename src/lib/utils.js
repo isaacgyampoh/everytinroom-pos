fbb9dd83626c04500eac3740d275eff20c567d8f
isaacgyampoh <nanagyams99@gmail.com>
2026-03-14 15:21:01 +0000

    Update receipt phone: 024 531 5581 / 024 936 5339

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -12,7 +12,7 @@ export const weekStartDate = () => format(startOfWeek(new Date(), { weekStartsOn
 export const SHOP = {
   name: 'EVERYTINROOM&BEDTIME',
   tagline: 'Your One Stop Shop',
-  phone: '054 812 4978 / 055 480 8341',
+  phone: '024 531 5581 / 024 936 5339',
   address: 'Adenta Aviation Road',
   website: 'www.Erbliving.shop',
   // Seasonal message on receipts (set to '' to remove)
